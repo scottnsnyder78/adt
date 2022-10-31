ace8e919ae614e26f6539b6771ac38c1c49454bd
Scott Snyder <scottnsnyder78@gmail.com>
2022-10-31 10:45:53 -0400

    Initial commit

---
+++ apps/frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'adt-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'frontend';
+}
