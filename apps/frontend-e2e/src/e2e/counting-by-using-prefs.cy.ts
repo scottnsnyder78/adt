86ac8d8b5fffade5f85af1c2360d03f1140cbb26
Scott Snyder <scottnsnyder78@gmail.com>
2022-11-02 09:36:49 -0400

    day 3 start

--- apps/frontend-e2e/src/e2e/counting-by-using-prefs.cy.ts
+++ apps/frontend-e2e/src/e2e/counting-by-using-prefs.cy.ts
@@ -18,4 +18,18 @@ describe('Counting by in Counter Uses Preferences', () => {
 
     counter.getCurrent().should('contain.text', '3');
   });
+  // More tests as needed - do it!
+  describe('what we want it to do', () => {
+    it('does not let me go below zero if the preferences are changed', () => {
+      nav.getPrefsLink().click();
+      prefs.getCountByThree().click();
+      nav.getCounterLink().click();
+      counter.getIncrement().click();
+      nav.getPrefsLink().click();
+      prefs.getCountByFive().click();
+      nav.getCounterLink().click();
+      counter.getDecrement().click();
+      counter.getCurrent().should('have.text', '0');
+    });
+  });
 });
